5b9a0dc45bd19bad0535b36a46a8f556a5c13a33
MLMatheus <61673626+MLMatheus@users.noreply.github.com>
2022-09-11 21:56:21 -0300

    Adicionada pocibilidade de geração de schema com arrays mesmo que contenha itens que não seja objetos; Corrigido alguns bugs; Adicionados tipos boleano e nullo; Efetuada refatoração no código

--- schema_generator.py
+++ schema_generator.py
@@ -1,35 +1,54 @@
 import json
 
-def generate(obj: dict, list_type=False):
+def  data_type(value):
+    dictionary = {
+        type(''): 'string',
+        type(54.0): 'number',
+        type({}): 'object',
+        type(54): 'integer',
+        type([]): 'array',
+        type(None): 'null',
+        type(True): 'boolean'
+    }
+
+    return dictionary[type(value)]
+
+def generate(
+    obj: dict,
+    list_type=False,
+    first=False
+):
     dictionary = {}
+
+    if first:
+        dictionary['$schema'] = 'https://json-schema.org/draft/2020-12/schema'
+        dictionary['type'] = 'object'
+        dictionary['properties'] = generate(obj)
+        dictionary['required'] = [k for k in obj.keys()]
+        return dictionary
 
     if list_type:
         dictionary['type'] = 'object'
         for item in obj:
+            if data_type(item) != 'object':
+                continue
             dictionary['properties'] = generate(item)
+            dictionary['required'] = [k for k in item.keys()]
         return dictionary
 
     for key in obj.keys():
-        data_type = {
-            type(''): 'string',
-            type(54.0): 'number',
-            type({}): 'object',
-            type(54): 'integer',
-            type([]): 'array'
-        }
-
         dic2 = {}
 
-        if data_type[type(obj[key])] == 'object':
+        if data_type(obj[key]) == 'object':
             dic2['type'] = 'object'
             dic2['properties'] = generate(obj[key])
-        elif data_type[type(obj[key])] == "array":
+            dic2['required'] = [k for k in obj.keys()]
+        elif data_type(obj[key]) == 'array':
             dic2['type'] = 'array'
             dic2['itens'] = generate(obj[key], True)
         else:
-            dic2['type'] = data_type[type(obj[key])]
+            dic2['type'] = data_type(obj[key])
         dictionary[key] = dic2
-    dictionary['required'] = [k for k in obj.keys()]
     return dictionary
 
 file = open("schema.json", "r")
@@ -37,7 +56,7 @@ file = open("schema.json", "r")
 schema_json = json.load(file)
 file.close()
 
-result = generate(schema_json)
+result = generate(schema_json, first=True)
 
 file = open("schema_result.json", "w")
 json.dump(result, file, indent=4)
